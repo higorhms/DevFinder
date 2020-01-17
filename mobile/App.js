a31f2524ce543977e6b0918d85805d7d8b59e8b0
higorhms <higorhmscebrac@hotmail.com>
2020-01-16 21:03:09 -0300

    Configurando o Prettier e o Eslint

--- mobile/App.js
+++ mobile/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Omnistack</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Hello Omnistack</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
